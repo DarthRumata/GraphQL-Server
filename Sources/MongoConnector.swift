15ed504d02c2d8087d205e6d6e0f0c18726a567a
Stas Kirichok <kirichok.stas@gmail.com>
2017-11-13 16:15:17 +0200

    [Change] Moved to cloud Mongo

--- Sources/MongoConnector.swift
+++ Sources/MongoConnector.swift
@@ -16,8 +16,11 @@ class MongoConnector {
   private lazy var historicalEventCollection: HistoricalEvent = HistoricalEvent()
 
   init() {
-    MongoDBConnection.host = "localhost"
-    MongoDBConnection.database = "local"
+    MongoDBConnection.host = "admin:TYU11bnm@cluster0-shard-00-00-c81ud.mongodb.net:27017,cluster0-shard-00-01-c81ud.mongodb.net:27017,cluster0-shard-00-02-c81ud.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+    MongoDBConnection.authdb = "admin"
+    MongoDBConnection.authmode = .standard
+    MongoDBConnection.username = "admin"
+    MongoDBConnection.database = "demo"
   }
 
   func getAllEvents() -> [HistoricalEvent] {
